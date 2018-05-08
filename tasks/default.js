9c1ee97135245a488aa7b83269b6b471dc10b5c1
alex <hamster2240@gmail.com>
2018-05-08 17:58:26 +0300

    Подключает svg-sprite

--- tasks/default.js
+++ tasks/default.js
@@ -2,26 +2,26 @@ import runSequence from 'run-sequence';
 import gulp from 'gulp';
 
 gulp.task('styles:dependencies', () => (
-	runSequence(
-		'sprite',
-		//'svg-sprite',
-	)
+  runSequence(
+    'sprite',
+    'svg-sprite',
+  )
 ));
 
 gulp.task('default', () => (
-	runSequence(
-		'clean',
-		'styles:dependencies',
-		['pug', 'bower', 'scripts'],
-		[
-			'styles:vendor',
-			'styles',
-			'copy-image',
-			'fonts',
-			'favicons',
-			'index-page'
-		],
-		'server',
-		'watch'
-	)
+  runSequence(
+    'clean',
+    'styles:dependencies',
+    ['pug', 'bower', 'scripts'],
+    [
+      'styles:vendor',
+      'styles',
+      'copy-image',
+      'fonts',
+      'favicons',
+      'index-page'
+    ],
+    'server',
+    'watch'
+  )
 ));
--- tasks/svg-sprite.js
+++ tasks/svg-sprite.js
@@ -1,24 +1,36 @@
-// import gulp from 'gulp';
-// import svgSprite from 'gulp-svg-sprites';
-// import plumber from 'gulp-plumber';
-// import fs from 'fs';
-//
-// gulp.task('svg-sprite', () => {
-//     gulp.src('./__dev/sprites/svg/*.svg')
-//         .pipe(svgSprite({
-//             cssFile: "../styles/helpers/08_svg-sprite.styl",
-//             svg: {
-//                 sprite: "../images/sprite-icons.svg"
-//             },
-//             svgPath: './images/sprite-icons.svg',
-//             preview: false,
-//             padding: 5,
-//             templates: {
-//                 css: fs.readFileSync(process.cwd() + '/__dev/styles/helpers/svg-sprite-template.css', "utf-8")
-//             },
-//             svgId: "svg-%f"
-//         }))
-//         .pipe(plumber())
-//         .pipe(gulp.dest('./__dev/sprites'))
-// });
-//
+import gulp from 'gulp';
+import config from '../config';
+import svgSprite from 'gulp-svg-sprites';
+import svgstore from 'gulp-svgstore';
+import cheerio from 'gulp-cheerio'
+import plumber from 'gulp-plumber';
+import fs from 'fs';
+
+gulp.task('svg-sprite', () => {
+    gulp.src('./__dev/sprites/svg/*.svg')
+        .pipe(svgSprite({
+            mode: "symbols",
+            cssFile: "../styles/helpers/08_svg-sprite.styl",
+            svg: {
+                symbols: "../images/sprite-icons.html"
+            },
+            svgPath: '../images/sprite-icons.svg',
+            preview: false,
+            templates: {
+                css: fs.readFileSync(process.cwd() + '/__dev/styles/helpers/svg-sprite-template.css', "utf-8")
+            },
+            svgId: "icon-%f"
+        })) 
+        .pipe(svgstore({ inlineSvg: true }))
+        .pipe(cheerio({
+        	run: function ($) {
+        		$('[fill]').removeAttr('fill');
+        		$('[stroke]').removeAttr('stroke');
+        		$('[title]').removeAttr('title');
+        		$('[data-name]').removeAttr('data-name');
+        	},
+        	parserOptions: {xmlMode: true}
+        }))
+        .pipe(plumber())
+        .pipe(gulp.dest('./__dev/images'))
+});
