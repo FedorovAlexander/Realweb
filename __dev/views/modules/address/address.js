51fa8b67ea39349cae88e83d93570e1a65588ed2
alex <hamster2240@gmail.com>
2019-02-12 15:32:45 +0300

    Refactor an address block js file

--- __dev/views/modules/address/address.js
+++ __dev/views/modules/address/address.js
@@ -1,30 +1,37 @@
 //Адрес
+let changeAddress = document.querySelector("#change-address");
+let addAddressBlock = document.querySelector(".address__add");
+let addAddressButton = document.querySelector(".address__add-btn");
+let removeAddressLink = document.querySelectorAll(".address__remove");
+let addressLabel = document.querySelectorAll(".address__label");
+let addressForm = document.querySelector(".address__form");
+let removeAddressLink1 = document.querySelector("#remove-address1");
+let removeAddressLink2 = document.querySelector("#remove-address2");
+let addressBlock0 = document.querySelector("#address-block-1");
+let addressBlock1 = document.querySelector("#address-block-2");
+let addressBlocks = document.querySelectorAll(".address__block");
 
-document.querySelector("#change-address").addEventListener("click", function() {
-  document.querySelector("#change-address").style.display = "none";
-  document.querySelector("#add-address-block").style.display = "block";
-  document.querySelector("#add-address-btn").style.display = "none";
-  document.querySelector("#remove-address1").style.display = "block";
-  document.querySelector("#remove-address2").style.display = "block";
-  document.querySelector("#address1").style.border = "1px solid #d5d5d5";
-  document.querySelector("#address2").style.border = "1px solid #d5d5d5";
-  document.querySelector("#address1").style.borderBottom = "none";
-  document.querySelector("#address1").style.backgroundColor = "#ffffff";
-  document.querySelector("#address2").style.backgroundColor = "#ffffff";
-  document.querySelector("#address__form").style.backgroundColor = "#f4f4f4";
-  document.querySelector("#address__form").style.paddingBottom = "0px";
-})
-document.querySelector("#remove-address1").addEventListener("click", function() {
-  document.querySelector("#first-address").style.display = "none";
-})
-document.querySelector("#remove-address2").addEventListener("click", function() {
-  document.querySelector("#second-address").style.display = "none";
-  document.querySelector("#address1").style.borderBottomColor = "#d5d5d5";
-})
-document.querySelector("#remove-address1").addEventListener("click", function() {
-  document.querySelector("#first-address").style.display = "none";
-})
-document.querySelector("#remove-address2").addEventListener("click", function() {
-  document.querySelector("#second-address").style.display = "none";
-  document.querySelector("#address1").style.borderBottomColor = "#d5d5d5";
-})
+
+changeAddress.addEventListener("click", function() {
+  this.style.display = "none";
+  addAddressBlock.style.display = "block";
+  addAddressButton.style.display = "none";
+
+  removeAddressLink.forEach(function(item) {
+    item.style.display = "block";
+  });
+
+  addressLabel.forEach(function(item) {
+    item.style.border = "1px solid #d5d5d5";
+    item.style.backgroundColor = "#ffffff";
+  });
+
+  addressForm.style.backgroundColor = "#f4f4f4";
+  addressForm.style.paddingBottom = "0px";
+});
+
+removeAddressLink.forEach(function(item,i) {
+  item.addEventListener('click', function() {
+    addressBlocks[i].style.display = 'none'
+  })
+});
