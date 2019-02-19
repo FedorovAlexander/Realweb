fd7122d4bb01ff16c5dc7bfae1cc213692e85f0e
alex <hamster2240@gmail.com>
2019-02-19 09:01:30 +0300

    Initial dist subtree commit

---
+++ template/scripts/personal-data.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function changeData() {
+    var dataItem = document.querySelectorAll('.personal-data__item');
+    dataItem.forEach(function(item) {
+
+        item.querySelector('.personal-data__change--change').addEventListener('click', function() {
+            item.querySelector('.personal-data__input').disabled = false;
+            item.style.backgroundColor = "#f4f4f4";
+            item.querySelector(".personal-data__change--change").style.display = "none";
+            item.querySelector(".personal-data__change--save").style.display = "block";
+        });
+
+        item.querySelector(".personal-data__change--save").addEventListener('click', function() {
+            item.querySelector('.personal-data__input').disabled = true;
+            item.style.backgroundColor = "#ffffff";
+            item.querySelector(".personal-data__change--change").style.display = "block";
+            item.querySelector(".personal-data__change--save").style.display = "none";
+        });
+    });
+}
+
+changeData();
