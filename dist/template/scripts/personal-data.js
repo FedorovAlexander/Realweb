81f9c646ff86bd09ace572c31713dbae9ebb2811
alex <hamster2240@gmail.com>
2019-02-22 15:27:34 +0300

    Deploy to the gh-pages

--- dist/template/scripts/personal-data.js
+++ dist/template/scripts/personal-data.js
@@ -6,14 +6,14 @@ function changeData() {
 
         item.querySelector('.personal-data__change--change').addEventListener('click', function() {
             item.querySelector('.personal-data__input').disabled = false;
-            item.style.backgroundColor = "#f4f4f4";
+            item.classList.add("personal-data__item--active");
             item.querySelector(".personal-data__change--change").style.display = "none";
             item.querySelector(".personal-data__change--save").style.display = "block";
         });
 
         item.querySelector(".personal-data__change--save").addEventListener('click', function() {
             item.querySelector('.personal-data__input').disabled = true;
-            item.style.backgroundColor = "#ffffff";
+            item.classList.remove("personal-data__item--active");
             item.querySelector(".personal-data__change--change").style.display = "block";
             item.querySelector(".personal-data__change--save").style.display = "none";
         });
@@ -21,3 +21,23 @@ function changeData() {
 }
 
 changeData();
+
+function hoverButton() {
+    var dataItem = document.querySelectorAll('.personal-data__item');
+    dataItem.forEach(function(item) {
+
+        item.querySelector('.personal-data__change--change').addEventListener('mouseover', function() {
+            item.style.backgroundColor = "#f4f4f4";
+            item.querySelector('.personal-data__input').style.backgroundColor = "#f4f4f4";
+            console.log('woof');
+        });
+
+        item.querySelector('.personal-data__change--change').addEventListener('mouseleave', function() {
+            item.style.backgroundColor = "#ffffff";
+            item.querySelector('.personal-data__input').style.backgroundColor = "#ffffff";
+            console.log('meow');
+        });
+    });
+}
+
+hoverButton();
