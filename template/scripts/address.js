c64fdd9922b2e8af270b506c8af3d28c5fa9a52a
alex <hamster2240@gmail.com>
2019-02-22 15:27:34 +0300

    Deploy to the gh-pages

--- template/scripts/address.js
+++ template/scripts/address.js
@@ -7,6 +7,7 @@ function addressBlock() {
     var addAddressButton = document.querySelector(".address__add-btn");
     var removeAddressLink = document.querySelectorAll(".address__remove");
     var addressLabel = document.querySelectorAll(".address__label");
+    var addressLabel1 = document.querySelector(".address__label:first-child");
     var addressForm = document.querySelector(".address__form");
     var addressBlocks = document.querySelectorAll(".address__block");
 
@@ -23,8 +24,10 @@ function addressBlock() {
         addressLabel.forEach(function(item) {
             item.style.border = "1px solid #d5d5d5";
             item.style.backgroundColor = "#ffffff";
+            item.style.paddingLeft = "12px";
         });
 
+        addressLabel1.style.borderBottom = "none";
         addressForm.style.backgroundColor = "#f4f4f4";
         addressForm.style.paddingBottom = "0px";
     });
@@ -32,6 +35,12 @@ function addressBlock() {
     removeAddressLink.forEach(function(item, i) {
         item.addEventListener('click', function() {
             addressBlocks[i].style.display = 'none';
+
+            if (addressLabel.length <= 1) {
+                addressLabel1.style.borderBottom = "none";
+            } else {
+                addressLabel1.style.borderBottom = "1px solid #d5d5d5";
+            }
         });
     });
 }
